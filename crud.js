d2ae20f4dd94b31eee04665e6baac3c6e9095f57
naufal afif <naufalafif58@gmail.com>
2019-02-09 22:56:44 +0700

    use jsonfile package on save json object

--- crud.js
+++ crud.js
@@ -1,16 +1,9 @@
+const file = "./dataset.json";
+
 const Schema = require('validate')
-
-let dataset = {
-    "notes": [{
-        id: 0,
-        content: 'Hello World!'
-    }],
-    "author": "naufalafif",
-    "email": "naufalafif58@gmail.com"
-}
-
-console.log('---- Start crud app ')
-console.log(`---- Total notes ${dataset.notes.length} `);
+const dataset = require(file);
+const jsonfile = require('jsonfile')
+const fs = require('fs');
 
 const createNoteId = () => {
     const id_lists = dataset.notes.map(i => i.id);
@@ -18,12 +11,18 @@ const createNoteId = () => {
     return max + 1;
 }
 
+const saveDataset = async (dataset_object) => {
+    jsonfile.writeFile(file, dataset_object, function (err) {
+        if (err) return false;
+        return true;
+    })
+}
+
 const getNoteIndex = (note_id) => {
     const id_lists = dataset.notes.map(i => i.id);
     const searched_index = id_lists.findIndex(id => id == note_id);
     return searched_index;
 }
-
 const notesSchema = new Schema({
     content: {
         type: String,
@@ -43,12 +42,16 @@ module.exports.createNote = (note) => {
         content: note,
         id: createNoteId()
     }
-
     const error = notesSchema.validate(note_to_save)[0];
-    if (!error)
+    if (!error) {
         dataset.notes.push(note_to_save);
-    else
+        saveDataset(dataset)
+        console.log('Note successfully added')
+        return true;
+    }else{
         console.log(`Error Messages : ${error.message} `)
+        return false;
+    }
 }
 
 module.exports.readNotes = () => dataset.notes;
@@ -57,22 +60,29 @@ module.exports.readNotes = () => dataset.notes;
 module.exports.updateNote = (note) => {
     const searched_note_index = getNoteIndex(note.id);
 
-    const error = notesSchema.validate(note_to_save)[0];
+    const error = notesSchema.validate(note)[0];
     if (!error) {
         if (searched_note_index != -1) {
             dataset.notes[searched_note_index].content = note.content;
+            saveDataset(dataset)
+            console.log('Note successfully updated')
+            return true;
         } else {
-            console.log(`Note does not exist`);
+            //console.log(`Note does not exist`);
         }
-    } else
-        console.log(`Error Messages : ${error.message} `)
+    }    
+    console.log(`Error Messages : ${error.message} `)
 }
 
 module.exports.deleteNote = (note_id) => {
     const index = getNoteIndex(note_id);
     if (index != -1) {
         dataset.notes.splice(index, 1);
-    } else {
-        console.log(`Note does not exist`);
+        saveDataset(dataset)
+        console.log('Note successfully deleted')
+        return true;
     }
+    console.log(`Note does not exist`);
+    return false;
+
 }
