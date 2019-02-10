e0716d51e2783e93cfd263b217178c7b0636075e
naufal afif <naufalafif58@gmail.com>
2019-02-10 19:35:02 +0700

    Remove jsonfile package & code, remove dataset file, use js object instead remove console features

--- app.js
+++ app.js
@@ -5,63 +5,12 @@ const {
     deleteNote
 } = require('./crud');
 
-const stdin = process.openStdin();
+console.log("create note\n");;
+console.log(createNote("New Note Content ..."));
+console.log("read note\n");;
+console.log(readNotes());
+console.log("read note\n");;
+console.log(updateNote({id:1,content:"Updated Note 1"}));
+console.log("delete note\n");;
+console.log(deleteNote(1));
 
-console.reset = function () {
-  return process.stdout.write('\033c');
-}
-
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
-  
-const header = ()=>{
-    console.log("\n\nCommand { r : Read, C : Create, U : Update, D : Delete, E : Exit, CLS : Clear Screen }\nthis app will wait for 5 second for each input");
-    process.stdout.write("Enter Command : ");
-}
-
-function sleep(milliSeconds){
-    var startTime = new Date().getTime();                    // get the current time
-    while (new Date().getTime() < startTime + milliSeconds); // hog cpu until time's up
-}
-
-header();
-stdin.addListener("data", function(command) {
-    const clean_command = command.toString().trim().toLowerCase();
-    
-    switch (clean_command) {
-        case 'r':
-            console.log(readNotes());
-            break;
-        case 'c':
-            readline.question(`Enter Note : `, (note) => {
-                createNote(note.toString());
-            })
-            sleep(5000);
-            break
-        case 'cls':
-            console.reset();
-            break;
-        case 'u':
-            // readline.question(`Enter Note ID & New Content (id|"note") : `, (note) => {
-            //     deleteNote(note);
-            // })
-            // sleep(5000);
-            break;
-        case 'd':
-            readline.question(`Enter Note Id : `, (note) => {
-                deleteNote(note);
-            })
-            sleep(5000);
-            break;
-        case 'e':
-            process.exit();
-            break;
-        default:
-            console.log('!!Command not valid')
-            break;
-    }
-    
-    header();
-});
--- crud.js
+++ crud.js
@@ -1,21 +1,10 @@
-const file = "./dataset.json";
-
 const Schema = require('validate')
-const dataset = require(file);
-const jsonfile = require('jsonfile')
-const fs = require('fs');
+let dataset = {"notes":[{"id":1,"contet":"Note 1","content":"Updated Note 1"},{"content":"New Note Content ...","id":2}]}
 
 const createNoteId = () => {
     const id_lists = dataset.notes.map(i => i.id);
     const max = Math.max(...id_lists);
     return max + 1;
-}
-
-const saveDataset = async (dataset_object) => {
-    jsonfile.writeFile(file, dataset_object, function (err) {
-        if (err) return false;
-        return true;
-    })
 }
 
 const getNoteIndex = (note_id) => {
@@ -45,7 +34,6 @@ module.exports.createNote = (note) => {
     const error = notesSchema.validate(note_to_save)[0];
     if (!error) {
         dataset.notes.push(note_to_save);
-        saveDataset(dataset)
         console.log('Note successfully added')
         return true;
     }else{
@@ -64,7 +52,6 @@ module.exports.updateNote = (note) => {
     if (!error) {
         if (searched_note_index != -1) {
             dataset.notes[searched_note_index].content = note.content;
-            saveDataset(dataset)
             console.log('Note successfully updated')
             return true;
         } else {
@@ -78,7 +65,6 @@ module.exports.deleteNote = (note_id) => {
     const index = getNoteIndex(note_id);
     if (index != -1) {
         dataset.notes.splice(index, 1);
-        saveDataset(dataset)
         console.log('Note successfully deleted')
         return true;
     }
