be3b8a9eb6f2177846c320e6f956ae5fe2ea9226
naufal afif <naufalafif58@gmail.com>
2019-02-10 07:23:35 +0700

    add create, update, delete

--- app.js
+++ app.js
@@ -6,13 +6,24 @@ const {
 } = require('./crud');
 
 const stdin = process.openStdin();
+
 console.reset = function () {
   return process.stdout.write('\033c');
 }
 
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  
 const header = ()=>{
-    console.log("\n\nCommand { r : Read, C : Create, U : Update, D : Delete, E : Exit, C : Clear Screen }");
+    console.log("\n\nCommand { r : Read, C : Create, U : Update, D : Delete, E : Exit, CLS : Clear Screen }\nthis app will wait for 5 second for each input");
     process.stdout.write("Enter Command : ");
+}
+
+function sleep(milliSeconds){
+    var startTime = new Date().getTime();                    // get the current time
+    while (new Date().getTime() < startTime + milliSeconds); // hog cpu until time's up
 }
 
 header();
@@ -24,10 +35,25 @@ stdin.addListener("data", function(command) {
             console.log(readNotes());
             break;
         case 'c':
+            readline.question(`Enter Note : `, (note) => {
+                createNote(note.toString());
+            })
+            sleep(5000);
+            break
+        case 'cls':
             console.reset();
             break;
-        case 'c':
-            // TODO
+        case 'u':
+            // readline.question(`Enter Note ID & New Content (id|"note") : `, (note) => {
+            //     deleteNote(note);
+            // })
+            // sleep(5000);
+            break;
+        case 'd':
+            readline.question(`Enter Note Id : `, (note) => {
+                deleteNote(note);
+            })
+            sleep(5000);
             break;
         case 'e':
             process.exit();
