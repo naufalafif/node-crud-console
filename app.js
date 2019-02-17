89f8dc710c1c81786d97666253ca71f1146076c3
naufalafif <naufalafif58@gmail.com>
2019-02-17 13:51:18 +0700

    masive update, add yargs etc

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
+#! /usr/bin/node
+
 const {
-    createNote,
-    readNotes,
-    updateNote,
-    deleteNote
+  createNote,
+  readNotes,
+  updateNote,
+  deleteNote
 } = require('./crud');
 
-console.log("create note\n");;
-console.log(createNote("New Note Content ..."));
-console.log("read note\n");;
-console.log(readNotes());
-console.log("read note\n");;
-console.log(updateNote({id:1,content:"Updated Note 1"}));
-console.log("delete note\n");;
-console.log(deleteNote(1));
+const yargs = require('yargs')
+let argv = yargs.argv;
 
+if (argv._.length) {
+  switch (argv._[0]) {
+    case 'read':
+      console.log(readNotes());
+      break;
+    case 'create':
+      if ("note" in argv)
+        if (createNote(argv.note))
+          console.log("note created");
+        else
+          console.log("Note Empty");
+      break;
+    case 'update':
+      if (updateNote({
+          note: argv.note,
+          id: argv.id
+        }))
+        console.log("note updated");
+      break;
+    case 'delete':
+      if ('id' in argv) {
+        if (deleteNote(argv.id))
+          console.log("note deleted");
+        else
+          console.log("note not found");
+
+      } else
+        console.log("Id Empty");
+      break;
+    default:
+      console.log("Command not valid");
+      break;
+  }
+} else console.log("Command Not Found");
--- crud.js
+++ crud.js
@@ -1,74 +1,125 @@
 const Schema = require('validate')
-let dataset = {"notes":[{"id":1,"contet":"Note 1","content":"Updated Note 1"},{"content":"New Note Content ...","id":2}]}
+let notes_data = {
+  "notes": [{
+    "id": 1,
+    "contet": "Note 1",
+    "content": "Updated Note 1"
+  }, {
+    "content": "New Note Content ...",
+    "id": 2
+  }]
+}
+const filepath = "./notes-data.json";
+const fs = require('fs');
+
+const notesSchema = new Schema({
+  note: {
+    type: String,
+    required: true,
+    length: {
+      min: 6
+    }
+  },
+  id: {
+    type: Number,
+    required: true
+  }
+})
+
+if (!fs.existsSync(filepath)) {
+  let notes_template = JSON.stringify({
+    notes: []
+  })
+  fs.writeFileSync(filepath, notes_template);
+}
 
 const createNoteId = () => {
-    const id_lists = dataset.notes.map(i => i.id);
+  let notes_data = readNotes();
+  const id_lists = notes_data.notes.map(i => i.id);
+  if (id_lists.length) {
     const max = Math.max(...id_lists);
     return max + 1;
+  }
+  return 0;
 }
 
 const getNoteIndex = (note_id) => {
-    const id_lists = dataset.notes.map(i => i.id);
+  let notes_data = readNotes();
+  const id_lists = notes_data.notes.map(i => i.id);
+  if (id_lists.length) {
     const searched_index = id_lists.findIndex(id => id == note_id);
     return searched_index;
-}
-const notesSchema = new Schema({
-    content: {
-        type: String,
-        required: true,
-        length: {
-            min: 6
-        }
-    },
-    id: {
-        type: Number,
-        required: true
-    }
-})
-
-module.exports.createNote = (note) => {
-    note_to_save = {
-        content: note,
-        id: createNoteId()
-    }
-    const error = notesSchema.validate(note_to_save)[0];
-    if (!error) {
-        dataset.notes.push(note_to_save);
-        console.log('Note successfully added')
-        return true;
-    }else{
-        console.log(`Error Messages : ${error.message} `)
-        return false;
-    }
+  }
+  return -1;
 }
 
-module.exports.readNotes = () => dataset.notes;
-
-
-module.exports.updateNote = (note) => {
-    const searched_note_index = getNoteIndex(note.id);
-
-    const error = notesSchema.validate(note)[0];
-    if (!error) {
-        if (searched_note_index != -1) {
-            dataset.notes[searched_note_index].content = note.content;
-            console.log('Note successfully updated')
-            return true;
-        } else {
-            //console.log(`Note does not exist`);
-        }
-    }    
-    console.log(`Error Messages : ${error.message} `)
-}
-
-module.exports.deleteNote = (note_id) => {
-    const index = getNoteIndex(note_id);
-    if (index != -1) {
-        dataset.notes.splice(index, 1);
-        console.log('Note successfully deleted')
-        return true;
-    }
-    console.log(`Note does not exist`);
+const saveNotesData = (notes) => {
+  try {
+    fs.writeFileSync(filepath, JSON.stringify(notes));
+    return true;
+  } catch (error) {
     return false;
+  }
+}
 
+const createNote = (note) => {
+  note_to_save = {
+    note: note,
+    id: createNoteId()
+  }
+  const error = notesSchema.validate(note_to_save)[0];
+  if (!error) {
+    let notes_data = readNotes();
+    notes_data.notes.push(note_to_save);
+    saveNotesData(notes_data);
+    return true;
+  } else {
+    console.log(`Error Messages : ${error.message} `)
+    return false;
+  }
+}
+
+const readNotes = () => {
+  let notes = JSON.parse(fs.readFileSync(filepath))
+  return notes;
+}
+
+const updateNote = (note) => {
+
+  const searched_note_index = getNoteIndex(note.id);
+
+  const error = notesSchema.validate(note)[0];
+  if (!error) {
+    if (searched_note_index != -1) {
+      let notes_to_save = readNotes();
+      notes_to_save.notes = notes_to_save.notes.map(i => {
+        if (i.id == note.id) i.note = note.note;
+        return i;
+      });
+      saveNotesData(notes_to_save);
+      return true;
+    } else {
+      console.log(`Note does not exist`);
+      return false;
+    }
+  }
+  console.log(`Error Messages : ${error.message} `)
+}
+
+const deleteNote = (note_id) => {
+  let notes_data = readNotes();
+  const current_length = notes_data.notes.length;
+  notes_data.notes = notes_data.notes.filter(i => i.id != note_id)
+  saveNotesData(notes_data);
+
+  if (notes_data.notes.length < current_length)
+    return true;
+  return false;
+}
+
+module.exports = {
+  readNotes,
+  createNote,
+  updateNote,
+  deleteNote
 }
